2aeec3767f5893e0a2d7c21d543810c5d8e8ff32
Daniel Berger <djberg96@gmail.com>
2012-04-26 11:10:37 -0600

    More conversion to FFI, though create_junction doesn't work yet.

--- lib/win32/dir.rb
+++ lib/win32/dir.rb
@@ -12,6 +12,7 @@ class Dir
   typedef :ulong, :dword
   typedef :ulong, :handle
   typedef :ulong, :hresult
+  typedef :ushort, :word
 
   ffi_lib :shell32
 
@@ -22,6 +23,20 @@ class Dir
   ffi_lib :shlwapi
 
   attach_function :PathIsDirectoryEmptyW, [:buffer_in], :bool
+
+  ffi_lib :kernel32
+
+  attach_function :CloseHandle, [:handle], :bool
+  attach_function :CreateDirectoryW, [:buffer_in, :pointer], :bool
+  attach_function :CreateFileW, [:buffer_in, :dword, :dword, :pointer, :dword, :dword, :handle], :handle
+  attach_function :DeviceIoControl, [:handle, :dword, :pointer, :dword, :pointer, :dword, :pointer, :pointer], :bool
+  attach_function :GetCurrentDirectoryW, [:dword, :buffer_out], :dword
+  attach_function :GetFileAttributesW, [:buffer_in], :dword
+  attach_function :GetLastError, [], :dword
+  attach_function :GetShortPathNameW, [:buffer_in, :buffer_out, :dword], :dword
+  attach_function :GetLongPathNameW, [:buffer_in, :buffer_out, :dword], :dword
+  attach_function :GetFullPathNameW, [:buffer_in, :dword, :buffer_out, :pointer], :dword
+  attach_function :RemoveDirectoryW, [:buffer_in], :bool
 
   # CSIDL constants
   csidl = Hash[
@@ -93,6 +108,20 @@ class Dir
     )
   end
 
+  # I fudge a bit, assuming a MountPointReparseBuffer
+  class REPARSE_JDATA_BUFFER < FFI::Struct
+    layout(
+      :ReparseTag, :ulong,
+      :ReparseDataLength, :ushort,
+      :Reserved, :ushort,
+      :SubstituteNameOffset, :ushort,
+      :SubstituteNameLength, :ushort,
+      :PrintNameOffset, :ushort,
+      :PrintNameLength, :ushort,
+      :PathBuffer, :pointer
+    )
+  end
+
   # Dynamically set each of the CSIDL constants
   csidl.each{ |key, value|
     buf  = 0.chr * 1024
@@ -122,56 +151,61 @@ class Dir
     MYDOCUMENTS = PERSONAL
   end
 
-=begin
   class << self
-    remove_method :getwd
-    remove_method :pwd
-    alias :old_glob :glob
-    alias :old_ref :[]
-    remove_method :glob
-    remove_method :[]
-  end
 
-  # Same as the standard MRI Dir.glob method except that it handles
-  # backslashes in path names.
-  #
-  def self.glob(glob_pattern, flags = 0, &block)
-    glob_pattern = glob_pattern.tr("\\", "/")
-    old_glob(glob_pattern, flags, &block)
-  end
+    # Same as the standard MRI Dir.glob method except that it handles
+    # backslashes in path names.
+    #
+    def glob(glob_pattern, flags = 0, &block)
+      glob_pattern = glob_pattern.tr("\\", "/")
+      old_glob(glob_pattern, flags, &block)
+    end
 
-  # Same as the standard MRI Dir[] method except that it handles
-  # backslashes in path names.
-  #
-  def self.[](glob_pattern)
-    glob_pattern = glob_pattern.tr("\\", "/")
-    old_ref(glob_pattern)
-  end
+    # Same as the standard MRI Dir[] method except that it handles
+    # backslashes in path names.
+    #
+    def self.[](glob_pattern)
+      glob_pattern = glob_pattern.tr("\\", "/")
+      old_ref(glob_pattern)
+    end
 
-  # Returns the present working directory. Unlike MRI, this method always
-  # normalizes the path.
-  #
-  # Examples:
-  #
-  #    Dir.chdir("C:/Progra~1")
-  #    Dir.getwd # => C:\Program Files
-  #
-  #    Dir.chdir("C:/PROGRAM FILES")
-  #    Dir.getwd # => C:\Program Files
-  #
-  def self.getwd
-    path1 = 0.chr * MAXPATH
-    path2 = 0.chr * MAXPATH
-    path3 = 0.chr * MAXPATH
+    # Returns the present working directory. Unlike MRI, this method always
+    # normalizes the path.
+    #
+    # Examples:
+    #
+    #    Dir.chdir("C:/Progra~1")
+    #    Dir.getwd # => C:\Program Files
+    #
+    #    Dir.chdir("C:/PROGRAM FILES")
+    #    Dir.getwd # => C:\Program Files
+    #
+    def getwd
+      path1 = 0.chr * 1024
+      path2 = 0.chr * 1024
+      path3 = 0.chr * 1024
 
-    GetCurrentDirectory(MAXPATH, path1)
-    GetShortPathName(path1, path2, MAXPATH)
-    GetLongPathName(path2, path3, MAXPATH)
+      path1.encode!('UTF-16LE')
 
-    path3[/^[^\0]*/]
-  end
+      if GetCurrentDirectoryW(path1.size, path1) == 0
+        raise SystemCallError, GetLastError(), "GetCurrentDirectoryW"
+      end
 
-  class << self
+      path2.encode!('UTF-16LE')
+
+      if GetShortPathNameW(path1, path2, path2.size) == 0
+        raise SystemCallError, GetLastError(), "GetShortPathNameW"
+      end
+
+      path3.encode!('UTF-16LE')
+
+      if GetLongPathNameW(path2, path3, path3.size) == 0
+        raise SystemCallError, GetLastError(), "GetLongPathNameW"
+      end
+
+      path3.strip
+    end
+
     alias :pwd :getwd
   end
 
@@ -187,90 +221,84 @@ class Dir
     to   = to.tr(File::SEPARATOR, File::ALT_SEPARATOR)   # Normalize path
     from = from.tr(File::SEPARATOR, File::ALT_SEPARATOR) # Normalize path
 
-    to_path    = 0.chr * MAXPATH
-    from_path  = 0.chr * MAXPATH
-    buf_target = 0.chr * MAXPATH
+    to_path    = 0.chr * 1024
+    from_path  = 0.chr * 1024
+    buf_target = 0.chr * 1024
 
-    length = GetFullPathName(from, from_path.size, from_path, 0)
+    from_path.encode!('UTF-16LE')
+
+    length = GetFullPathNameW(from.encode('UTF-16LE'), from_path.size, from_path, nil)
 
     if length == 0
-      raise StandardError, 'GetFullPathName() failed: ' + get_last_error
+      raise SystemCallError, GetLastError(), "GetFullPathNameW"
     else
-      from_path = from_path[0..length-1]
+      from_path.strip!
     end
 
-    length = GetFullPathName(to, to_path.size, to_path, 0)
+    to_path.encode!('UTF-16LE')
+
+    length = GetFullPathNameW(to.encode('UTF-16LE'), to_path.size, to_path, nil)
 
     if length == 0
-      raise StandardError, 'GetFullPathName() failed: ' + get_last_error
+      raise SystemCallError, GetLastError(), "GetFullPathNameW"
     else
-      to_path = to_path[0..length-1]
+      to_path.strip!
     end
 
     # You can create a junction to a directory that already exists, so
     # long as it's empty.
-    rv = CreateDirectory(to_path, 0)
+    rv = CreateDirectoryW(to_path, nil)
 
-    if rv == 0 && rv != ERROR_ALREADY_EXISTS
-      raise StandardError, 'CreateDirectory() failed: ' + get_last_error
+    if rv == 0 && rv != 183 # ERROR_ALREADY_EXISTS
+      raise SystemCallError, GetLastError(), "CreateDirectoryW"
     end
-
-    handle = CreateFile(
-      to_path,
-      GENERIC_READ | GENERIC_WRITE,
-      0,
-      0,
-      OPEN_EXISTING,
-      FILE_FLAG_OPEN_REPARSE_POINT | FILE_FLAG_BACKUP_SEMANTICS,
-      0
-    )
-
-    if handle == INVALID_HANDLE_VALUE
-      raise StandardError, 'CreateFile() failed: ' + get_last_error
-    end
-
-    buf_target  = buf_target.split(0.chr).first
-    buf_target  = "\\??\\" << from_path
-    wide_string = multi_to_wide(buf_target)[0..-3]
-
-    # REPARSE_JDATA_BUFFER
-    rdb = [
-      "0xA0000003L".hex,      # ReparseTag (IO_REPARSE_TAG_MOUNT_POINT)
-      wide_string.size + 12,  # ReparseDataLength
-      0,                      # Reserved
-      0,                      # SubstituteNameOffset
-      wide_string.size,       # SubstituteNameLength
-      wide_string.size + 2,   # PrintNameOffset
-      0,                      # PrintNameLength
-      wide_string             # PathBuffer
-    ].pack('LSSSSSSa' + (wide_string.size + 4).to_s)
-
-    bytes = [0].pack('L')
 
     begin
-      bool = DeviceIoControl(
-        handle,
-        CTL_CODE(FILE_DEVICE_FILE_SYSTEM, 41, METHOD_BUFFERED, 0),
-        rdb,
-        rdb.size,
-        0,
-        0,
-        bytes,
-        0
-      )
+      # Generic read & write + open existing + reparse point & backup semantics
+      handle = CreateFileW(to_path, 3221225472, 0, nil, 3, 35651584, 0)
 
-      unless bool
-        error = 'DeviceIoControl() failed: ' + get_last_error
-        RemoveDirectory(to_path)
-        raise error
+      if handle == 0xFFFFFFFF # INVALID_HANDLE_VALUE
+        raise SystemCallError, GetLastError(), "CreateFileW"
       end
-    ensure
-      CloseHandle(handle)
+
+      target = "\\??\\".encode('UTF-16LE') << from_path
+
+      rdb = REPARSE_JDATA_BUFFER.new
+      rdb[:ReparseTag] = 2684354563 # IO_REPARSE_TAG_MOUNT_POINT
+      rdb[:ReparseDataLength] = target.size + 12
+      rdb[:Reserved] = 0
+      rdb[:SubstituteNameOffset] = 0
+      rdb[:SubstituteNameLength] = target.size
+      rdb[:PrintNameOffset] = target.size + 2
+      rdb[:PrintNameLength] = 0
+      rdb[:PathBuffer] = FFI::MemoryPointer.from_string(target)
+
+      bytes = FFI::MemoryPointer.new(:ulong)
+
+      begin
+        bool = DeviceIoControl(
+          handle,
+          CTL_CODE(9, 41, 0, 0),
+          rdb,
+          rdb.size,
+          nil,
+          0,
+          bytes,
+          nil
+        )
+
+        unless bool
+          error = GetLastError()
+          RemoveDirectoryW(to_path)
+          raise SystemCallError, error, "DeviceIoControl"
+        end
+      ensure
+        CloseHandle(handle)
+      end
     end
 
     self
   end
-=end
 
   # Returns whether or not +path+ is empty.  Returns false if +path+ is not
   # a directory, or contains any files other than '.' or '..'.
@@ -281,18 +309,18 @@ class Dir
     PathIsDirectoryEmptyW(path)
   end
 
-=begin
   # Returns whether or not +path+ is a junction.
   #
   def self.junction?(path)
-    bool   = true
-    attrib = GetFileAttributes(path)
+    bool = true
+    path = path << "\0"
+    path.encode!('UTF-16LE')
 
-    if attrib == INVALID_FILE_ATTRIBUTES ||
-       attrib & FILE_ATTRIBUTE_DIRECTORY == 0 ||
-       attrib & FILE_ATTRIBUTE_REPARSE_POINT == 0
-    then
-       bool = false
+    attrib = GetFileAttributesW(path)
+
+    # Only directories with a reparse point attribute can be junctions
+    if (attrib == 0xFFFFFFFF) || (attrib & 0x00000010 == 0) || (attrib & 0x00000400 == 0)
+      bool = false
     end
 
     bool
@@ -303,5 +331,10 @@ class Dir
   class << self
     alias reparse_dir? junction?
   end
-=end
+
+  private
+
+  def self.CTL_CODE(device, function, method, access)
+    ((device) << 16) | ((access) << 14) | ((function) << 2) | (method)
+  end
 end
