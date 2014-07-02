0cdde1f20858f10ae0ec16fd988ee0aafd7d5699
Daniel Berger <djberg96@gmail.com>
2014-07-02 09:18:11 -0500

    Version bump.

--- test/test_win32_dir.rb
+++ test/test_win32_dir.rb
@@ -26,7 +26,7 @@ class TC_Win32_Dir < Test::Unit::TestCase
   end
 
   test "version number is set to expected value" do
-    assert_equal('0.4.8', Dir::VERSION)
+    assert_equal('0.4.9', Dir::VERSION)
   end
 
   test 'glob handles backslashes' do
