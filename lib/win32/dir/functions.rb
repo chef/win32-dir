8cb9ee91b0560731837bbf4351baef03e3ad7274
Tim Smith <tsmith84@gmail.com>
2020-08-17 11:16:07 -0700

    Resolve chefstyle warnings
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/win32/dir/functions.rb
+++ lib/win32/dir/functions.rb
@@ -25,9 +25,9 @@ module Dir::Functions
   ffi_lib :shell32
   ffi_convention :stdcall
 
-  attach_pfunc :SHGetFolderPathW, [:hwnd, :int, :handle, :dword, :buffer_out], :dword
-  attach_pfunc :SHGetFolderLocation, [:hwnd, :int, :handle, :dword, :ptr], :dword
-  attach_pfunc :SHGetFileInfo, [:dword, :dword, :ptr, :uint, :uint], :dword
+  attach_pfunc :SHGetFolderPathW, %i{hwnd int handle dword buffer_out}, :dword
+  attach_pfunc :SHGetFolderLocation, %i{hwnd int handle dword ptr}, :dword
+  attach_pfunc :SHGetFileInfo, %i{dword dword ptr uint uint}, :dword
 
   ffi_lib :shlwapi
   ffi_convention :stdcall
@@ -38,15 +38,15 @@ module Dir::Functions
   ffi_convention :stdcall
 
   attach_pfunc :CloseHandle, [:handle], :bool
-  attach_pfunc :CreateDirectoryW, [:buffer_in, :ptr], :bool
-  attach_pfunc :CreateFileW, [:buffer_in, :dword, :dword, :ptr, :dword, :dword, :handle], :handle
-  attach_pfunc :DeviceIoControl, [:handle, :dword, :ptr, :dword, :ptr, :dword, :ptr, :ptr], :bool
-  attach_pfunc :GetCurrentDirectoryW, [:dword, :buffer_out], :dword
+  attach_pfunc :CreateDirectoryW, %i{buffer_in ptr}, :bool
+  attach_pfunc :CreateFileW, %i{buffer_in dword dword ptr dword dword handle}, :handle
+  attach_pfunc :DeviceIoControl, %i{handle dword ptr dword ptr dword ptr ptr}, :bool
+  attach_pfunc :GetCurrentDirectoryW, %i{dword buffer_out}, :dword
   attach_pfunc :GetFileAttributesW, [:buffer_in], :dword
   attach_pfunc :GetLastError, [], :dword
-  attach_pfunc :GetShortPathNameW, [:buffer_in, :buffer_out, :dword], :dword
-  attach_pfunc :GetLongPathNameW, [:buffer_in, :buffer_out, :dword], :dword
-  attach_pfunc :GetFullPathNameW, [:buffer_in, :dword, :buffer_out, :ptr], :dword
+  attach_pfunc :GetShortPathNameW, %i{buffer_in buffer_out dword}, :dword
+  attach_pfunc :GetLongPathNameW, %i{buffer_in buffer_out dword}, :dword
+  attach_pfunc :GetFullPathNameW, %i{buffer_in dword buffer_out ptr}, :dword
   attach_pfunc :RemoveDirectoryW, [:buffer_in], :bool
 end
 
@@ -61,6 +61,6 @@ class String
   # any remaining null characters.
   def wstrip
     force_encoding("UTF-16LE").encode("UTF-8", invalid: :replace, undef: :replace)
-    .split("\x00")[0].encode(Encoding.default_external)
+      .split("\x00")[0].encode(Encoding.default_external)
   end
 end
